1a5b76ea7f01b1d0fd21c828eb98132f3be26311
amirdzafiic <adzafic1@etf.unsa.ba>
2026-05-18 13:38:29 +0200

    Promjena u ApplicationDbContextu

--- ezZkvi/OOAD projekat v2/Data/ApplicationDbContext.cs
+++ ezZkvi/OOAD projekat v2/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using ezZkvi.Models;
 
 namespace ezZkvi.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<Korisnik>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
